5ac145d03adc20ac84e12db5c17ef20c73420890
github username <github email address>
2019-04-20 18:36:20 +0200

    used yield to iterate over array

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,15 @@
+def my_collect(array)
+	if block_given?
+		i = 0
+		arr = []
 
+		while i < array.length
+			arr << yield(array[i])
+			i += 1
+		end
+		arr
+	else
+		puts "empty collection"
+	end
+end
 
